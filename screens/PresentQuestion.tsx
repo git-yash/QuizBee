9172f298bd01bdd1712690bfbd1932b50f14abb2
yashm <yashmittalshah@gmail.com>
2021-06-28 16:32:28 -0500

    fixed steal UI and made alert cancellable.

--- screens/PresentQuestion.tsx
+++ screens/PresentQuestion.tsx
@@ -3,23 +3,23 @@ import {
   Button,
   Container,
   Content,
-  Footer,
+  Footer, Icon,
   ListItem,
   Radio,
   Text,
   Toast,
-  View,
-} from 'native-base';
-import React, {useEffect, useState} from 'react';
-import {Alert, StyleSheet} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
-import Player from '../models/Player';
-import {decode} from 'html-entities';
+  View
+} from "native-base";
+import React, { useEffect, useState } from "react";
+import { Alert, StyleSheet, TouchableOpacity } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import Player from "../models/Player";
+import { decode } from "html-entities";
 
-const PresentQuestion = ({navigation, route}) => {
-  const {question, players, categories, currentPlayerRef} = route.params;
+const PresentQuestion = ({ navigation, route }) => {
+  const { question, players, categories, currentPlayerRef } = route.params;
   const [selectedAnswer, setSelectedAnswer] = useState<string>(
-    question.attempted_answer,
+    question.attempted_answer
   );
   const [currentPlayer, setCurrentPlayer] = useState<Player>(currentPlayerRef);
 
@@ -37,26 +37,27 @@ const PresentQuestion = ({navigation, route}) => {
         key={player.id}
         style={{
           backgroundColor:
-            currentPlayer?.id === player.id ? '#f0ad4e' : 'white',
-          borderRightColor: 'lightgray',
+            currentPlayer?.id === player.id ? "#f0ad4e" : "white",
+          borderRightColor: "lightgray",
           borderRightWidth: 1,
           flex: 1,
           padding: 10,
           borderTopWidth: 1,
-          borderTopColor: 'lightgray',
+          borderTopColor: "lightgray",
           borderBottomWidth: 1,
-          borderBottomColor: 'lightgray',
+          borderBottomColor: "lightgray"
         }}>
-        <Text style={{alignSelf: 'center', fontWeight: 'bold'}}>
+        <Text style={{ alignSelf: "center", fontWeight: "bold" }}>
           {player.name}
         </Text>
-        <Text style={{alignSelf: 'center', fontWeight: 'bold', fontSize: 22}}>
+        <Text style={{ alignSelf: "center", fontWeight: "bold", fontSize: 22 }}>
           {player.score}
         </Text>
         {!question.attempted_answer && currentPlayer?.id !== player.id && (
-          <Button onPress={() => stealQuestion(player)}>
-            <Text>Steal</Text>
-          </Button>
+          <TouchableOpacity onPress={() => stealQuestion(player)}>
+            <Icon style={{ textAlign: "center", color: "#3F51B5" }} name="shuffle" /><Text
+            style={{ fontSize: 13, textAlign: "center", color: "#3F51B5" }}>Steal</Text>
+          </TouchableOpacity>
         )}
       </View>
     );
@@ -65,18 +66,18 @@ const PresentQuestion = ({navigation, route}) => {
   const createAnswerChoiceItem = (id: number, answerChoice: string) => {
     const getColor = () => {
       if (!question.attempted_answer) {
-        return 'black';
+        return "black";
       } else if (question.correct_answer === answerChoice) {
-        return '#89d469';
+        return "#89d469";
       } else if (selectedAnswer === answerChoice) {
-        return '#f19191';
+        return "#f19191";
       } else {
-        return 'black';
+        return "black";
       }
     };
 
     return (
-      <ListItem key={id} onPress={() => setSelectedAnswer(answerChoice)}>
+      <ListItem key={id} onPress={() => !question.attempted_answer && setSelectedAnswer(answerChoice)}>
         {!question.attempted_answer && (
           <Radio
             selected={selectedAnswer === answerChoice}
@@ -84,7 +85,7 @@ const PresentQuestion = ({navigation, route}) => {
           />
         )}
         <Body>
-          <Text style={{color: getColor()}}>{decode(answerChoice)}</Text>
+          <Text style={{ color: getColor() }}>{decode(answerChoice)}</Text>
         </Body>
       </ListItem>
     );
@@ -95,7 +96,7 @@ const PresentQuestion = ({navigation, route}) => {
       <ScrollView>
         <Content>
           <View
-            style={{display: 'flex', flexDirection: 'row', marginBottom: 10}}>
+            style={{ display: "flex", flexDirection: "row", marginBottom: 10 }}>
             {players.map((p: Player) => createPlayerItem(p))}
           </View>
           <Text
@@ -103,49 +104,49 @@ const PresentQuestion = ({navigation, route}) => {
               fontSize: 22,
               padding: 10,
               marginBottom: 20,
-              marginTop: 20,
+              marginTop: 20
             }}>
             {decode(question.question)}
           </Text>
           {question
             .getOptions()
             .map((o: string, index: number) =>
-              createAnswerChoiceItem(index, o),
+              createAnswerChoiceItem(index, o)
             )}
         </Content>
       </ScrollView>
       {!question.attempted_answer && (
         <Footer>
           <Button
-            style={{alignSelf: 'center', padding: 20}}
+            style={{ alignSelf: "center", padding: 20 }}
             onPress={() => {
               if (!selectedAnswer) {
-                Alert.alert('Invalid selection', 'Please select an answer');
+                Alert.alert("Invalid selection", "Please select an answer", undefined, { cancelable: true });
                 return;
               }
 
               question.attempted_answer = selectedAnswer;
               currentPlayer.answerQuestion(question);
               Toast.show({
-                text: question.isCorrectAnswer() ? 'Correct' : 'Incorrect! The correct answer was ' + question.correct_answer + '.',
-                type: question.isCorrectAnswer() ? 'success' : 'danger',
-                duration: question.isCorrectAnswer() ? 1500 : 3000,
+                text: question.isCorrectAnswer() ? "Correct" : "Incorrect! The correct answer was " + question.correct_answer + ".",
+                type: question.isCorrectAnswer() ? "success" : "danger",
+                duration: question.isCorrectAnswer() ? 1500 : 3000
               });
 
-              navigation.navigate('SelectQuestion', {
-                name: 'Select Question',
+              navigation.navigate("SelectQuestion", {
+                name: "Select Question",
                 players: players,
                 categories: categories,
-                answeredQuestion: question,
+                answeredQuestion: question
               });
             }}>
-            <Text>Submit</Text>
-          </Button>
-        </Footer>
-      )}
-    </Container>
-  );
-};
+              <Text>Submit</Text>
+              </Button>
+              </Footer>
+              )}
+        </Container>
+      );
+      };
 
 const styles = StyleSheet.create({});
 
